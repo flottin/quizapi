4beb9a586d0b4a294e115a9f4b5a6c16274d7c5b
Florent LOTTIN <flottin@gmail.com>
2018-11-04 13:51:00 +0100

    add controller test fixtures

---
+++ src/Entity/Question.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Entity;
+
+//use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\QuestionRepository")
+ */
+class Question
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $text;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text)
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+}
