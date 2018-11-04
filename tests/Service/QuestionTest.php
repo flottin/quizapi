c49a3eca02078a2513e7a1746ca3633d9f6a9169
Florent LOTTIN <flottin@gmail.com>
2018-11-04 15:52:49 +0100

    factorise mock

--- tests/Service/QuestionTest.php
+++ tests/Service/QuestionTest.php
@@ -11,7 +11,6 @@ class QuestionTest extends TestCase
 {
     public function testQuestionsPresents()
     {
-        // add test
         $q = new Question();
         $q->setText('test');
         $questions [] = $q;
@@ -20,39 +19,23 @@ class QuestionTest extends TestCase
         $q->setText('test');
         $questions [] = $q;
 
-        // Now, mock the repository so it returns the mock of the employee
-        $repository = $this->createMock(ObjectRepository::class);
-        // use getMock() on PHPUnit 5.3 or below
-        // $employeeRepository = $this->getMock(ObjectRepository::class);
-        $repository->expects($this->any())
-            ->method('findBy')
-            ->willReturn($questions);
 
-        // Last, mock the EntityManager to return the mock of the repository
-        $objectManager = $this->createMock(ObjectManager::class);
-        // use getMock() on PHPUnit 5.3 or below
-        // $objectManager = $this->getMock(ObjectManager::class);
-        $objectManager->expects($this->any())
-            ->method('getRepository')
-            ->willReturn($repository);
-
-
-        $c = new Questions($objectManager);
-
-
+        $om = self::getObjectManager ($questions);
+        $c = new Questions($om);
         $result = $c->getQuestions ();
-
-
         $this->assertEquals(count($result), 2);
     }
 
     public function testQuestionsNotPresents()
     {
-
         $questions = [];
+        $om = self::getObjectManager ($questions);
+        $c = new Questions($om);
+        $result = $c->getQuestions ();
+        $this->assertEquals(count($result), 0);
+    }
 
-
-
+    public function getObjectManager($questions){
         // Now, mock the repository so it returns the mock of the employee
         $repository = $this->createMock(ObjectRepository::class);
         // use getMock() on PHPUnit 5.3 or below
@@ -69,11 +52,7 @@ class QuestionTest extends TestCase
             ->method('getRepository')
             ->willReturn($repository);
 
-
-        $c = new Questions($objectManager);
-        $result = $c->getQuestions ();
-//var_dump($result);
-        $this->assertEquals(count($result), 0);
+        return $objectManager;
     }
 
 }
